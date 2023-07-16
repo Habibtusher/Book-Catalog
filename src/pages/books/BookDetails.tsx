9c0e2d3483beb0accf060486eaf20c85bfe68ead
Habibtusher <tusherhrt@gmail.com>
2023-07-16 18:32:16 +0600

    add review and view review

--- src/pages/books/BookDetails.tsx
+++ src/pages/books/BookDetails.tsx
@@ -1,28 +1,36 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useAddReviewMutation, useSingleBookQuery } from "../../redux/features/book/bookApi";
+import {
+  useAddReviewMutation,
+  useSingleBookQuery,
+} from "../../redux/features/book/bookApi";
 import Loading from "../shared/Loading";
 import { useAppSelector } from "../../redux/hooks";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 const BookDetails = () => {
   const { id } = useParams();
   const [reviewText, setReviewText] = useState<string>();
-  console.log(
-    "🚀 ~ file: BookDetails.tsx:10 ~ BookDetails ~ reviewText:",
-    reviewText
-  );
-  const { data, error, isLoading } = useSingleBookQuery(id);
+
+  const { data, isLoading } = useSingleBookQuery(id);
   const { user } = useAppSelector((state) => state.user);
-  const [addReview] = useAddReviewMutation()
+  const [addReview, { isSuccess, data: addReviewData }] =
+    useAddReviewMutation();
 
   const handleAddReview = () => {
     const data = {
       id: id,
       data: { review: reviewText },
     };
-    addReview(data)
+    addReview(data);
   };
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success(addReviewData?.message);
+      setReviewText("");
+    }
+  }, [isSuccess, addReviewData?.message]);
   if (isLoading) {
     return <Loading />;
   }
@@ -50,6 +58,7 @@ const BookDetails = () => {
             {user?.email && (
               <div className="pt-28 flex items-center gap-10">
                 <textarea
+                  value={reviewText}
                   onChange={(e) => setReviewText(e.target.value)}
                   placeholder="Write review..."
                   className="textarea textarea-bordered textarea-lg w-full max-w-xs"
@@ -64,6 +73,18 @@ const BookDetails = () => {
             )}
           </div>
         </div>
+      </div>
+      <div className="pt-10">
+        {data?.data.reviews.map((review: string) => (
+          <div className="flex items-center gap-4">
+            <div className="avatar">
+              <div className="w-10 rounded-full">
+                <img src="https://w7.pngwing.com/pngs/81/570/png-transparent-profile-logo-computer-icons-user-user-blue-heroes-logo-thumbnail.png" />
+              </div>
+            </div>
+            <p>{review}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
