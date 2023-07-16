53483d230789e7a47b5f6f6f227dc5df86711910
Habibtusher <tusherhrt@gmail.com>
2023-07-16 22:27:37 +0600

    add deployed server link

---
+++ src/constant/Url.ts
@@ -0,0 +1,2 @@
+export const baseUrl = "https://book-catalog-server-topaz.vercel.app";
+// export const baseUrl = "http://localhost:5000/;
--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { baseUrl } from '../../constant/Url';
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/v1' }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${baseUrl}/api/v1`}),
   tagTypes: ['newBook','addReview'],
   endpoints: () => ({}),
 });
