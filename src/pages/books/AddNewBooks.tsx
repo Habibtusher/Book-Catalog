96a77905a6af7a49cb9280fc2d4ba30338a166b0
Habibtusher <tusherhrt@gmail.com>
2023-07-16 18:39:50 +0600

    add new book button update

--- src/pages/books/AddNewBooks.tsx
+++ src/pages/books/AddNewBooks.tsx
@@ -14,7 +14,6 @@ type FormInputs = {
 };
 
 const AddNewBooks = () => {
- 
   const {
     register,
     handleSubmit,
@@ -67,7 +66,6 @@ const AddNewBooks = () => {
     { value: "Nonfiction", title: "Nonfiction" },
   ];
   useEffect(() => {
-    
     if (!isLoading && isSuccess) {
       toast.success(data?.message);
       reset();
@@ -76,64 +74,71 @@ const AddNewBooks = () => {
     if (!isLoading && isError && error) {
       toast.error(error?.data.errorMessages[0].message);
     }
-  }, [isLoading, isSuccess, isError, error, data,reset]);
+  }, [isLoading, isSuccess, isError, error, data, reset]);
   return (
-    <div className="container mx-auto">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-4">
-          <label className="block text-gray-700">Title:</label>
-          <input
-            className="border border-gray-300 rounded-md px-4 py-2 w-full"
-            {...register("title", { required: true })}
-          />
-          {errors.title && (
-            <span className="text-red-500">Title is required.</span>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700">Author:</label>
-          <input
-            className="border border-gray-300 rounded-md px-4 py-2 w-full"
-            {...register("author", { required: true })}
-          />
-          {errors.author && (
-            <span className="text-red-500">Author is required.</span>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700">Genre:</label>
-          <select
-            className="border border-gray-300 rounded-md px-4 py-2 w-full"
-            {...register("genre", { required: true })}
+    <div className="w-3/6 mx-auto pb-16">
+      <div className="">
+        <p className="text-2xl pb-10 text-center">Add New Book</p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-4">
+            <label className="block text-gray-700">Title:</label>
+            <input
+              className="border border-gray-300 rounded-md px-4 py-2 w-full"
+              {...register("title", { required: true })}
+            />
+            {errors.title && (
+              <span className="text-red-500">Title is required.</span>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Author:</label>
+            <input
+              className="border border-gray-300 rounded-md px-4 py-2 w-full"
+              {...register("author", { required: true })}
+            />
+            {errors.author && (
+              <span className="text-red-500">Author is required.</span>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Genre:</label>
+            <select
+              className="border border-gray-300 rounded-md px-4 py-2 w-full"
+              {...register("genre", { required: true })}
+            >
+              {genres.map((genre) => (
+                <option key={genre.value} value={genre.value}>
+                  {genre.title}
+                </option>
+              ))}
+            </select>
+            {errors.genre && (
+              <span className="text-red-500">Genre is required.</span>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700">Publication Date:</label>
+            <input
+              type="date"
+              className="border border-gray-300 rounded-md px-4 py-2 w-full"
+              {...register("publicationDate", { required: true })}
+            />
+            {errors.publicationDate && (
+              <span className="text-red-500">
+                Publication Date is required.
+              </span>
+            )}
+          </div>
+          <div className="text-right ">
+          <button
+            type="submit"
+            className="btn-neutral  text-white font-bold py-2 px-4 rounded"
           >
-            {genres.map((genre) => (
-              <option key={genre.value} value={genre.value}>
-                {genre.title}
-              </option>
-            ))}
-          </select>
-          {errors.genre && (
-            <span className="text-red-500">Genre is required.</span>
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700">Publication Date:</label>
-          <input
-            type="date"
-            className="border border-gray-300 rounded-md px-4 py-2 w-full"
-            {...register("publicationDate", { required: true })}
-          />
-          {errors.publicationDate && (
-            <span className="text-red-500">Publication Date is required.</span>
-          )}
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Submit
-        </button>
-      </form>
+            Submit
+          </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
